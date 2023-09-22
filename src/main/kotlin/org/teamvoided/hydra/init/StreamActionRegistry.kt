6bc815c13a0ce79117d0924ed4cf197de71fbbad
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-09-22 19:54:39 -0400

    Added RandomMob action

--- src/main/kotlin/org/teamvoided/hydra/init/StreamActionRegistry.kt
+++ src/main/kotlin/org/teamvoided/hydra/init/StreamActionRegistry.kt
@@ -2,13 +2,14 @@ package org.teamvoided.hydra.init
 
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnGroup
 import net.minecraft.entity.SpawnReason
+import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
-import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Identifier
-import org.teamvoided.hydra.StreamAction
 import org.teamvoided.hydra.Hydra
+import org.teamvoided.hydra.StreamAction
 
 object StreamActionRegistry {
     val STREAM_ACTION_KEY = RegistryKey.ofRegistry<StreamAction>(Identifier(Hydra.MODID, "stream_action"))
@@ -16,7 +17,22 @@ object StreamActionRegistry {
         FabricRegistryBuilder.createSimple(STREAM_ACTION_KEY).buildAndRegister()
 
     val SPAWN_MOB = register("spawn_mob", StreamAction {
-        EntityType.PIG.spawn(it.world as ServerWorld?, it.blockPos, SpawnReason.EVENT)
+        EntityType.PIG.spawn(it.serverWorld, it.blockPos, SpawnReason.EVENT)
+    })
+
+    val SPAWN_RANDOM_MOB = register("spawn_random_mob", StreamAction {
+        fun spawnRandom() {
+            Registries.ENTITY_TYPE.getRandom(it.random).ifPresent { it1 ->
+                val type = it1.value();
+                if (type.spawnGroup != SpawnGroup.MISC) {
+                    type.spawn(it.serverWorld, it.blockPos, SpawnReason.EVENT)
+                } else {
+                    spawnRandom();
+                }
+            }
+        }
+        
+        spawnRandom()
     })
 
     fun init() {
