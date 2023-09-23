9c26ac2cb67b80f0d1f5aaf93ae8f9f878f5b7ef
theendercore <theendercore@gmail.com>
2023-09-23 04:51:02 +0300

    - Finished ChannelPointsEventC2S

--- src/main/kotlin/org/teamvoided/hydra/networking/packages/c2s/ChannelPointsEventC2S.kt
+++ src/main/kotlin/org/teamvoided/hydra/networking/packages/c2s/ChannelPointsEventC2S.kt
@@ -4,12 +4,17 @@ import io.netty.buffer.Unpooled
 import net.minecraft.network.PacketByteBuf
 import org.teamvoided.hydra.networking.packages.HydraPackage
 
-class ChannelPointsEventC2S(val stuff: String) : HydraPackage {
-    constructor(buf: PacketByteBuf) : this(buf.readString())
+class ChannelPointsEventC2S(
+    val id: String, val displayName: String, val userInput: String, val status: String,
+) : HydraPackage {
+    constructor(buf: PacketByteBuf) : this(buf.readString(), buf.readString(), buf.readString(), buf.readString())
 
     override fun write(): PacketByteBuf {
         val buf = PacketByteBuf(Unpooled.buffer())
-        buf.writeString(stuff)
+        buf.writeString(id)
+        buf.writeString(displayName)
+        buf.writeString(userInput)
+        buf.writeString(status)
         return buf
     }
 }
--- src/main/kotlin/org/teamvoided/hydra/twitch/EventListeners.kt
+++ src/main/kotlin/org/teamvoided/hydra/twitch/EventListeners.kt
@@ -7,5 +7,15 @@ import org.teamvoided.hydra.networking.packages.c2s.ChannelPointsEventC2S
 
 
 fun channelPointsEvent(event: RewardRedeemedEvent) {
-    ClientPlayNetworking.send(NetworkManager.CHANNEL_POINTS_EVENT, ChannelPointsEventC2S("asd").write())
+    val data = event.redemption
+    println(data.cursor)
+    ClientPlayNetworking.send(
+        NetworkManager.CHANNEL_POINTS_EVENT,
+        ChannelPointsEventC2S(
+            data.reward.id,
+            data.user.displayName,
+            if (data.userInput != null) data.userInput else "",
+            data.status
+        ).write()
+    )
 }
