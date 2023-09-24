b015001266d91c449ce23f547d96ccf7e29df2a2
wired-tomato <brokenfuse.self@gmail.com>
2023-09-24 12:32:26 -0400

    Packet stuff

--- src/main/kotlin/org/teamvoided/hydra/twitch/EventListeners.kt
+++ src/main/kotlin/org/teamvoided/hydra/twitch/EventListeners.kt
@@ -2,23 +2,23 @@ package org.teamvoided.hydra.twitch
 
 import com.github.twitch4j.pubsub.events.FollowingEvent
 import com.github.twitch4j.pubsub.events.RewardRedeemedEvent
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
-import org.teamvoided.hydra.networking.NetworkManager
 import org.teamvoided.hydra.networking.packages.c2s.ChannelPointsEventC2S
+import org.teamvoided.hydra.networking.packages.c2s.ChannelPointsEventC2SPacket
 import org.teamvoided.hydra.networking.packages.c2s.FollowingEventC2S
+import org.teamvoided.hydra.networking.packages.c2s.FollowingEventC2SPacket
+import org.teamvoided.voidlib.networking.Packets.sendToServer
 
 
 fun channelPointsEvent(event: RewardRedeemedEvent) {
     val data = event.redemption
     println(data.cursor)
-    ClientPlayNetworking.send(
-        NetworkManager.CHANNEL_POINTS_EVENT,
+    ChannelPointsEventC2SPacket.sendToServer(
         ChannelPointsEventC2S(
             data.reward.id, data.user.displayName, if (data.userInput != null) data.userInput else "", data.status
-        ).write()
+        )
     )
 }
 
 fun followingEvent(event: FollowingEvent) {
-    ClientPlayNetworking.send(NetworkManager.FOLLOWING_EVENT, FollowingEventC2S(event.data.displayName).write())
+    FollowingEventC2SPacket.sendToServer(FollowingEventC2S(event.data.displayName))
 }
