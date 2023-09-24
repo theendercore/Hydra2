9653bd1d5252fe6707a67214fdcf33adf798d693
wired-tomato <brokenfuse.self@gmail.com>
2023-09-24 11:32:56 -0400

    Other packet

--- src/main/kotlin/org/teamvoided/hydra/networking/packages/c2s/FollowingEventC2S.kt
+++ src/main/kotlin/org/teamvoided/hydra/networking/packages/c2s/FollowingEventC2S.kt
@@ -1,14 +1,35 @@
 package org.teamvoided.hydra.networking.packages.c2s
 
-import io.netty.buffer.Unpooled
-import net.minecraft.network.PacketByteBuf
-import org.teamvoided.hydra.networking.packages.HydraPackage
+import kotlinx.serialization.BinaryFormat
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.cbor.Cbor
+import net.fabricmc.fabric.api.networking.v1.PacketSender
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.network.ServerPlayNetworkHandler
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+import org.teamvoided.hydra.networking.NetworkManager.FOLLOWING_EVENT
+import org.teamvoided.voidlib.networking.ChainC2SVoidPacket
 
-data class FollowingEventC2S(val displayName: String,) : HydraPackage {
-    constructor(buf: PacketByteBuf) : this(buf.readString())
-    override fun write(): PacketByteBuf {
-        val buf = PacketByteBuf(Unpooled.buffer())
-        buf.writeString(displayName)
-        return buf
+@Serializable
+data class FollowingEventC2S(val displayName: String)
+
+object FollowingEventC2SPacket: ChainC2SVoidPacket<FollowingEventC2S> {
+    override val chainId: Identifier = FOLLOWING_EVENT
+    @OptIn(ExperimentalSerializationApi::class)
+    override val format: BinaryFormat = Cbor { encodeDefaults = true }
+    override val serializer: KSerializer<FollowingEventC2S> = FollowingEventC2S.serializer()
+
+    override fun handleC2S(
+        server: MinecraftServer,
+        player: ServerPlayerEntity,
+        handler: ServerPlayNetworkHandler,
+        data: FollowingEventC2S,
+        responseSender: PacketSender
+    ) {
+        player.sendMessage(Text.of("${data.displayName} has followed you!"), false)
     }
 }
